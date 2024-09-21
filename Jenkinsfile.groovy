9825a4abf07952b501ebc984eed86df28b82ffa4
LuoYunXiao <1829462342@qq.com>
2024-09-21 17:40:17 +0800

    test mail

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -50,15 +50,13 @@ pipeline {
                 success {
                     emailext subject: "SUCCESS: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
                             body: "The job ${env.JOB_NAME} has completed successfully.",
-                            recipientProviders: [developers(), requestor()],
-                            to: requestor()
+                            recipientProviders: [developers(), requestor()]
 
                 }
                 failure {
                     emailext subject: "FAILURE: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
                             body: "The job ${env.JOB_NAME} has failed. Check console output for details: ${env.BUILD_URL}",
-                            recipientProviders: [developers(), requestor()],
-                            to: requestor()
+                            recipientProviders: [developers(), requestor()]
 
                 }
             }
