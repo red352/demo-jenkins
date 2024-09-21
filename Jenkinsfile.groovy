526847eb7addc96a2de3cddf54989c814560bbf2
LuoYunXiao <1829462342@qq.com>
2024-09-21 16:10:43 +0800

    test deploy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -42,6 +42,7 @@ pipeline {
                         jenkins_dir=\$(realpath --relative-to=/var/jenkins_home \$(pwd))
                         ssh root@192.168.31.114 << EOF
                             base_dir=/etc/config/jenkins/jenkins_home/\${jenkins_dir} &&
+                            echo base_dir=\$base_dir &&
                             cd \$base_dir &&
                             docker buildx build -t ${env.IMAGE_NAME}:${env.IMAGE_TAG} . &&
                             docker compose up -d 
